9db120610d96f9e3a95e1990b7c550bae3e717c7
Ibrahima Ciss <bionik6@gmail.com>
2018-08-03 04:07:09 +0000

    Add tests for SearchRequest

---
+++ MovieDBSearchTests/Network/SearchRequestTests.swift
@@ -0,0 +1,45 @@
+//
+//  SearchRequestTests.swift
+//  MovieDBSearchTests
+//
+//  Created by Ibrahima Ciss on 8/3/18.
+//  Copyright © 2018 Ibrahima Ciss. All rights reserved.
+//
+
+import XCTest
+@testable import MovieDBSearch
+
+class SearchRequestTests: XCTestCase {
+    
+    private var sut: SearchRequest!
+    
+    override func setUp() {
+        super.setUp()
+        sut = SearchRequest(searchTerms: "avengers")
+    }
+    
+    func testSutHasCorrectPath() {
+        let expectedPath = "search/movie"
+        XCTAssertEqual(sut.path, expectedPath)
+    }
+    
+    func testSutHasCorrectMethod() {
+        XCTAssertEqual(sut.method, .get)
+    }
+    
+    func testSutHasCorrectParams() {
+        switch sut.params! {
+        case .url(let params):
+            XCTAssertEqual(params["query"] as! String, "avengers")
+            XCTAssertNotNil(params["api_key"])
+        default: break
+        }
+    }
+    
+    func testSutHasNoHeaders() {
+        XCTAssertNil(sut.headers)
+    }
+    
+  
+    
+}
